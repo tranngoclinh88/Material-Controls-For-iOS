728cbdeb9869ac93a334dad9ad9481f35172d0c5
Bach Pham <pxbachs@gmail.com>
2016-10-17 10:08:48 +0700

    Rename NS to MaterialControls

---
+++ MaterialControls/MaterialControls/MaterialControls.h
@@ -0,0 +1,13 @@
+//
+//  MaterialControls.h
+//  MaterialControls
+//
+//  Created by BachPX1 on 10/13/16.
+//  Copyright © 2016 FPT Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MaterialControls : NSObject
+
+@end
