95173c2dc101aedfc9adc9b8beb8944291f00d21
Bach Pham <pxbachs@gmail.com>
2016-10-18 10:06:10 +0700

    Removed old files

--- Xamarin-Sample/XamarinUILib/libiOSUILib.linkwith.cs
+++
@@ -1,4 +0,0 @@
-using System;
-using ObjCRuntime;
-
-[assembly: LinkWith ("libiOSUILib.a", LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.Arm64, SmartLink = true,Frameworks = "CoreText QuartzCore", ForceLoad = true)]
