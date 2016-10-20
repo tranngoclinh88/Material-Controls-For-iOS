f7c16b23482d2eb1b6f273f4b5d118ca8782ed8f
Aleksey Nemychenkov <alexejnemychenkov@gmail.com>
2016-10-20 18:07:13 +0300

    Fixed retain cicle between MDRippleLayer and MDTableViewCell. Fixed new MDRippleLayers constantly adding to cell sublayers

--- MaterialControls/MaterialControls/MDTableViewCell.m
+++ MaterialControls/MaterialControls/MDTableViewCell.m
@@ -67,6 +67,10 @@
 }
 
 - (void)initLayer {
+  if (_mdLayer) {
+    [_mdLayer removeFromSuperlayer];
+    _mdLayer = nil;
+  }
   if (!_rippleColor)
     _rippleColor = [UIColor colorWithWhite:0.5 alpha:1];
 
