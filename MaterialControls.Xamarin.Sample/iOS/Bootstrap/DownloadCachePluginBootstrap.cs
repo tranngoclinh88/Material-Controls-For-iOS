82442a729941fc30e671379f9400b33d4f4ee105
LinhTN <LinhTN@altplus.com.vn>
2017-06-07 16:45:01 +0700

    Remove unused classes.

--- MaterialControls.Xamarin.Sample/iOS/Bootstrap/DownloadCachePluginBootstrap.cs
+++
@@ -1,9 +0,0 @@
-using MvvmCross.Platform.Plugins;
-
-namespace MaterialControls.Xamarin.Sample.iOS.Bootstrap
-{
-    public class DownloadCachePluginBootstrap
-        : MvxLoaderPluginBootstrapAction<MvvmCross.Plugins.DownloadCache.PluginLoader, MvvmCross.Plugins.DownloadCache.iOS.Plugin>
-    {
-    }
-}
